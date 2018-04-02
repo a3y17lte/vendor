1d74e332fce4926e72fc01e11a47704326816833
Matt Grf <g.matthieu49@gmail.com>
2018-04-02 11:28:39 +0200

    a3y17lte: vendor: New style

--- a3y17lte-vendor-blobs.mk
+++ a3y17lte-vendor-blobs.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2017-2018 The LineageOS Project
+# Copyright (C) 2018 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -16,8 +16,109 @@
 
 LOCAL_PATH := vendor/samsung/a3y17lte/proprietary/
 
+#==/ Audio /==#
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)lib/hw/audio_amplifier.universal7870.so:system/lib/hw/audio_amplifier.universal7870.so \
+    $(LOCAL_PATH)etc/Tfa9896.cnt:system/etc/Tfa9896.cnt \
+    $(LOCAL_PATH)lib/libtfa98xx.so:system/lib/libtfa98xx.so
+
+#==/ Bluetooth /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)lib/libbt-vendor.so:system/lib/libbt-vendor.so \
+    $(LOCAL_PATH)bin/wcnss_filter:system/bin/wcnss_filter \
+    $(LOCAL_PATH)etc/bluetooth/av_performance.conf:system/etc/bluetooth/av_performance.conf \
+    $(LOCAL_PATH)etc/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
+    $(LOCAL_PATH)etc/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
+    $(LOCAL_PATH)etc/bluetooth/iop_bt.db:system/etc/bluetooth/iop_bt.db \
+    $(LOCAL_PATH)etc/bluetooth/iop_device_list.conf:system/etc/bluetooth/iop_device_list.conf
+
+#==/ Wifi /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)etc/firmware/Data.msc:system/etc/firmware/Data.msc \
+    $(LOCAL_PATH)etc/firmware/bdwlan30.bin:system/etc/firmware/bdwlan30.bin \
+    $(LOCAL_PATH)etc/firmware/bdwlan32.bin:system/etc/firmware/bdwlan32.bin \
+    $(LOCAL_PATH)etc/firmware/nvm_tlv.bin:system/etc/firmware/nvm_tlv.bin \
+    $(LOCAL_PATH)etc/firmware/nvm_tlv_1.3.bin:system/etc/firmware/nvm_tlv_1.3.bin \
+    $(LOCAL_PATH)etc/firmware/nvm_tlv_2.1.bin:system/etc/firmware/nvm_tlv_2.1.bin \
+    $(LOCAL_PATH)etc/firmware/nvm_tlv_3.0.bin:system/etc/firmware/nvm_tlv_3.0.bin \
+    $(LOCAL_PATH)etc/firmware/nvm_tlv_3.2.bin:system/etc/firmware/nvm_tlv_3.2.bin \
+    $(LOCAL_PATH)etc/firmware/nvm_tlv_tf_1.1.bin:system/etc/firmware/nvm_tlv_tf_1.1.bin \
+    $(LOCAL_PATH)etc/firmware/otp30.bin:system/etc/firmware/otp30.bin \
+    $(LOCAL_PATH)etc/firmware/qwlan30.bin:system/etc/firmware/qwlan30.bin \
+    $(LOCAL_PATH)etc/firmware/qwlan30_ibss.bin:system/etc/firmware/qwlan30_ibss.bin \
+    $(LOCAL_PATH)etc/firmware/rampatch_tlv.img:system/etc/firmware/rampatch_tlv.img \
+    $(LOCAL_PATH)etc/firmware/rampatch_tlv_1.3.tlv:system/etc/firmware/rampatch_tlv_1.3.tlv \
+    $(LOCAL_PATH)etc/firmware/rampatch_tlv_2.1.tlv:system/etc/firmware/rampatch_tlv_2.1.tlv \
+    $(LOCAL_PATH)etc/firmware/rampatch_tlv_3.0.tlv:system/etc/firmware/rampatch_tlv_3.0.tlv \
+    $(LOCAL_PATH)etc/firmware/rampatch_tlv_3.2.tlv:system/etc/firmware/rampatch_tlv_3.2.tlv \
+    $(LOCAL_PATH)etc/firmware/rampatch_tlv_tf_1.1.tlv:system/etc/firmware/rampatch_tlv_tf_1.1.tlv \
+    $(LOCAL_PATH)etc/firmware/utf30.bin:system/etc/firmware/utf30.bin \
+    $(LOCAL_PATH)etc/firmware/utfbd30.bin:system/etc/firmware/utfbd30.bin \
+    $(LOCAL_PATH)etc/firmware/utfbd32.bin:system/etc/firmware/utfbd32.bin \
+    $(LOCAL_PATH)lib/modules/qca_cld/qca_cld_wlan.ko:system/lib/modules/qca_cld/qca_cld_wlan.ko
+	
+ #==/ Camera ==/#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)lib/hw/camera.vendor.universal7870.so:system/lib/hw/camera.vendor.universal7870.so \
+    $(LOCAL_PATH)lib/libcsc.so:system/lib/libcsc.so \
+    $(LOCAL_PATH)lib/libexynoscamera.so:system/lib/libexynoscamera.so \
+    $(LOCAL_PATH)lib/libexynoscamera3.so:system/lib/libexynoscamera3.so \
+    $(LOCAL_PATH)lib/libseccameracore.so:system/lib/libseccameracore.so \
+    $(LOCAL_PATH)lib/libuniplugin.so:system/lib/libuniplugin.so \
+    $(LOCAL_PATH)lib/libhwjpeg.so:system/lib/libhwjpeg.so \
+    $(LOCAL_PATH)lib/libexynosutils.so:system/lib/libexynosutils.so \
+    $(LOCAL_PATH)lib/libexynosgscaler.so:system/lib/libexynosgscaler.so \
+    $(LOCAL_PATH)lib/libexynosscaler.so:system/lib/libexynosscaler.so \
+    $(LOCAL_PATH)lib/libexynosv4l2.so:system/lib/libexynosv4l2.so \
+    $(LOCAL_PATH)lib/libstainkiller.so:system/lib/libstainkiller.so \
+    $(LOCAL_PATH)lib/libHpr_RecFace_dl_v1.0.so:system/lib/libHpr_RecFace_dl_v1.0.so \
+    $(LOCAL_PATH)lib/libHpr_RecGAE_cvFeature_v1.0.so:system/lib/libHpr_RecGAE_cvFeature_v1.0.so \
+    $(LOCAL_PATH)lib/libHpr_TaskFaceClustering_hierarchical_v1.0.so:system/lib/libHpr_TaskFaceClustering_hierarchical_v1.0.so \
+    $(LOCAL_PATH)vendor/firmware/setfile_imx219.bin:system/vendor/firmware/setfile_imx219.bin \
+    $(LOCAL_PATH)vendor/firmware/setfile_imx258.bin:system/vendor/firmware/setfile_imx258.bin \
+    $(LOCAL_PATH)lib/libOpenCv.so:system/lib/libOpenCv.so \
+    $(LOCAL_PATH)vendor/lib/libHpr_LocLandmark_sdm_v1.0.so:system/vendor/lib/libHpr_LocLandmark_sdm_v1.0.so \
+    $(LOCAL_PATH)lib/libsomp.so:system/lib/libsomp.so \
+    $(LOCAL_PATH)vendor/lib/libHpr_LocFace_mvfd_v1.0.so:system/vendor/lib/libHpr_LocFace_mvfd_v1.0.so \
+    $(LOCAL_PATH)vendor/lib/libAutoEnhance.so:system/vendor/lib/libAutoEnhance.so \
+    $(LOCAL_PATH)vendor/lib/libPhotoEnhance.so:system/vendor/lib/libPhotoEnhance.so
+
+#==/ DRM /==# 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
+    $(LOCAL_PATH)vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
+
+#== / Fingerprint /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)lib/hw/fingerprint.default.so:lib/hw/fingerprint.default.so \
+    $(LOCAL_PATH)lib/libbauthserver.so:system/lib/libbauthserver.so \
+    $(LOCAL_PATH)lib/libbauthtzcommon.so:system/lib/libbauthtzcommon.so \
+    $(LOCAL_PATH)lib/libegis_fp_normal_sensor_test.so:system/lib/libegis_fp_normal_sensor_test.so \
+    $(LOCAL_PATH)lib/libsynaFpSensorTestNwd.so:system/lib/libsynaFpSensorTestNwd.so
+	
+ #==/ GPS /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)bin/gpsd:system/bin/gpsd \
+    $(LOCAL_PATH)etc/ca.pem:system/etc/ca.pem \
+    $(LOCAL_PATH)lib/hw/gps.default.so:system/lib/hw/gps.default.so \
+    $(LOCAL_PATH)lib/libfloatingfeature.so:system/lib/libfloatingfeature.so \
+    $(LOCAL_PATH)lib/libwrappergps.so:system/lib/libwrappergps.so \
+    $(LOCAL_PATH)vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
+    $(LOCAL_PATH)vendor/lib/libflp.so:system/vendor/lib/libflp.so 
+
+#==/ Graphics /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)lib/egl/egl.cfg:system/lib/egl/egl.cfg \
+    $(LOCAL_PATH)lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so
+
+#==/ Media /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)vendor/lib/libSEF.so:system/vendor/lib/libSEF.so \
+    $(LOCAL_PATH)vendor/firmware/fimc_is_lib.bin:system/vendor/firmware/fimc_is_lib.bin \
+    $(LOCAL_PATH)vendor/firmware/mfc_fw.bin:system/vendor/firmware/mfc_fw.bin
+
+#==/ Mobicore /==#
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)app/FFFFFFFF000000000000000000000001.drbin:system/app/FFFFFFFF000000000000000000000001.drbin \
     $(LOCAL_PATH)app/mcRegistry/00060308060501020000000000000000.tlbin:system/app/mcRegistry/00060308060501020000000000000000.tlbin \
     $(LOCAL_PATH)app/mcRegistry/07010000000000000000000000000000.tlbin:system/app/mcRegistry/07010000000000000000000000000000.tlbin \
@@ -48,99 +149,42 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)app/mcRegistry/ffffffffd00000000000000000000017.tlbin:system/app/mcRegistry/ffffffffd00000000000000000000017.tlbin \
     $(LOCAL_PATH)app/mcRegistry/fffffffff0000000000000000000001b.tlbin:system/app/mcRegistry/fffffffff0000000000000000000001b.tlbin \
     $(LOCAL_PATH)app/mcRegistry/fffffffff0000000000000000000001e.tlbin:system/app/mcRegistry/fffffffff0000000000000000000001e.tlbin \
-    $(LOCAL_PATH)bin/gpsd:system/bin/gpsd \
     $(LOCAL_PATH)bin/mcDriverDaemon:system/bin/mcDriverDaemon \
-    $(LOCAL_PATH)bin/sensorhubservice:system/bin/sensorhubservice \
-    $(LOCAL_PATH)bin/wcnss_filter:system/bin/wcnss_filter \
-    $(LOCAL_PATH)etc/Tfa9896.cnt:system/etc/Tfa9896.cnt \
-    $(LOCAL_PATH)etc/bluetooth/av_performance.conf:system/etc/bluetooth/av_performance.conf \
-    $(LOCAL_PATH)etc/bluetooth/bt_did.conf:system/etc/bluetooth/bt_did.conf \
-    $(LOCAL_PATH)etc/bluetooth/bt_stack.conf:system/etc/bluetooth/bt_stack.conf \
-    $(LOCAL_PATH)etc/bluetooth/iop_bt.db:system/etc/bluetooth/iop_bt.db \
-    $(LOCAL_PATH)etc/bluetooth/iop_device_list.conf:system/etc/bluetooth/iop_device_list.conf \
-    $(LOCAL_PATH)etc/ca.pem:system/etc/ca.pem \
-    $(LOCAL_PATH)etc/firmware/Data.msc:system/etc/firmware/Data.msc \
-    $(LOCAL_PATH)etc/firmware/bdwlan30.bin:system/etc/firmware/bdwlan30.bin \
-    $(LOCAL_PATH)etc/firmware/bdwlan32.bin:system/etc/firmware/bdwlan32.bin \
-    $(LOCAL_PATH)etc/firmware/nvm_tlv.bin:system/etc/firmware/nvm_tlv.bin \
-    $(LOCAL_PATH)etc/firmware/nvm_tlv_1.3.bin:system/etc/firmware/nvm_tlv_1.3.bin \
-    $(LOCAL_PATH)etc/firmware/nvm_tlv_2.1.bin:system/etc/firmware/nvm_tlv_2.1.bin \
-    $(LOCAL_PATH)etc/firmware/nvm_tlv_3.0.bin:system/etc/firmware/nvm_tlv_3.0.bin \
-    $(LOCAL_PATH)etc/firmware/nvm_tlv_3.2.bin:system/etc/firmware/nvm_tlv_3.2.bin \
-    $(LOCAL_PATH)etc/firmware/nvm_tlv_tf_1.1.bin:system/etc/firmware/nvm_tlv_tf_1.1.bin \
-    $(LOCAL_PATH)etc/firmware/otp30.bin:system/etc/firmware/otp30.bin \
-    $(LOCAL_PATH)etc/firmware/qwlan30.bin:system/etc/firmware/qwlan30.bin \
-    $(LOCAL_PATH)etc/firmware/qwlan30_ibss.bin:system/etc/firmware/qwlan30_ibss.bin \
-    $(LOCAL_PATH)etc/firmware/rampatch_tlv.img:system/etc/firmware/rampatch_tlv.img \
-    $(LOCAL_PATH)etc/firmware/rampatch_tlv_1.3.tlv:system/etc/firmware/rampatch_tlv_1.3.tlv \
-    $(LOCAL_PATH)etc/firmware/rampatch_tlv_2.1.tlv:system/etc/firmware/rampatch_tlv_2.1.tlv \
-    $(LOCAL_PATH)etc/firmware/rampatch_tlv_3.0.tlv:system/etc/firmware/rampatch_tlv_3.0.tlv \
-    $(LOCAL_PATH)etc/firmware/rampatch_tlv_3.2.tlv:system/etc/firmware/rampatch_tlv_3.2.tlv \
-    $(LOCAL_PATH)etc/firmware/rampatch_tlv_tf_1.1.tlv:system/etc/firmware/rampatch_tlv_tf_1.1.tlv \
-    $(LOCAL_PATH)etc/firmware/utf30.bin:system/etc/firmware/utf30.bin \
-    $(LOCAL_PATH)etc/firmware/utfbd30.bin:system/etc/firmware/utfbd30.bin \
-    $(LOCAL_PATH)etc/firmware/utfbd32.bin:system/etc/firmware/utfbd32.bin \
-    $(LOCAL_PATH)etc/sec_s3nrn80_rfreg.bin:system/etc/sec_s3nrn80_rfreg.bin \
-    $(LOCAL_PATH)lib/egl/egl.cfg:system/lib/egl/egl.cfg \
-    $(LOCAL_PATH)lib/egl/libGLES_android.so:system/lib/egl/libGLES_android.so \
-    $(LOCAL_PATH)lib/hw/camera.vendor.universal7870.so:system/lib/hw/camera.vendor.universal7870.so \
-    $(LOCAL_PATH)lib/hw/gps.default.so:system/lib/hw/gps.default.so \
-    $(LOCAL_PATH)lib/hw/gralloc.exynos5.so:system/lib/hw/gralloc.exynos5.so \
-    $(LOCAL_PATH)lib/hw/nfc_nci.universal7870.so:system/lib/hw/nfc_nci.universal7870.so \
-    $(LOCAL_PATH)lib/hw/sensorhubs.universal7870.so:system/lib/hw/sensorhubs.universal7870.so \
-    $(LOCAL_PATH)lib/hw/sensors.universal7870.so:system/lib/hw/sensors.universal7870.so \
-    $(LOCAL_PATH)lib/libHpr_RecFace_dl_v1.0.so:system/lib/libHpr_RecFace_dl_v1.0.so \
-    $(LOCAL_PATH)lib/libHpr_RecGAE_cvFeature_v1.0.so:system/lib/libHpr_RecGAE_cvFeature_v1.0.so \
-    $(LOCAL_PATH)lib/libHpr_TaskFaceClustering_hierarchical_v1.0.so:system/lib/libHpr_TaskFaceClustering_hierarchical_v1.0.so \
-    $(LOCAL_PATH)lib/libaudio-ril.so:system/lib/libaudio-ril.so \
-    $(LOCAL_PATH)lib/libbauthserver.so:system/lib/libbauthserver.so \
-    $(LOCAL_PATH)lib/libbauthtzcommon.so:system/lib/libbauthtzcommon.so \
-    $(LOCAL_PATH)lib/libbt-vendor.so:system/lib/libbt-vendor.so \
-    $(LOCAL_PATH)lib/libcsc.so:system/lib/libcsc.so \
-    $(LOCAL_PATH)lib/libegis_fp_normal_sensor_test.so:system/lib/libegis_fp_normal_sensor_test.so \
-    $(LOCAL_PATH)lib/hw/fingerprint.default.so:lib/hw/fingerprint.default.so \
-    $(LOCAL_PATH)lib/libexynoscamera.so:system/lib/libexynoscamera.so \
-    $(LOCAL_PATH)lib/libexynoscamera3.so:system/lib/libexynoscamera3.so \
-    $(LOCAL_PATH)lib/libexynosgscaler.so:system/lib/libexynosgscaler.so \
-    $(LOCAL_PATH)lib/libexynosscaler.so:system/lib/libexynosscaler.so \
-    $(LOCAL_PATH)lib/libexynosutils.so:system/lib/libexynosutils.so \
-    $(LOCAL_PATH)lib/libexynosv4l2.so:system/lib/libexynosv4l2.so \
-    $(LOCAL_PATH)lib/libfloatingfeature.so:system/lib/libfloatingfeature.so \
-    $(LOCAL_PATH)lib/libhwjpeg.so:system/lib/libhwjpeg.so \
-    $(LOCAL_PATH)lib/libprotobuf-cpp-N.so:system/lib/libprotobuf-cpp-N.so \
-    $(LOCAL_PATH)lib/libprotobuf-cpp-fooo.so:system/lib/libprotobuf-cpp-fooo.so \
-    $(LOCAL_PATH)lib/libreference-ril.so:system/lib/libreference-ril.so \
-    $(LOCAL_PATH)lib/librilutils.so:system/lib/librilutils.so \
+    $(LOCAL_PATH)lib/libMcClient.so:system/lib/libMcClient.so \
+    $(LOCAL_PATH)lib/libMcRegistry.so:system/lib/libMcRegistry.so
+
+#==/ Radio /==#
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)lib/libsec-ril-dsds.so:system/lib/libsec-ril-dsds.so \
     $(LOCAL_PATH)lib/libsec-ril.so:system/lib/libsec-ril.so \
-    $(LOCAL_PATH)lib/libseccameracore.so:system/lib/libseccameracore.so \
-    $(LOCAL_PATH)lib/libsecnativefeature.so:system/lib/libsecnativefeature.so \
     $(LOCAL_PATH)lib/libsecril-client.so:system/lib/libsecril-client.so \
-    $(LOCAL_PATH)lib/libsensorhub.so:system/lib/libsensorhub.so \
-    $(LOCAL_PATH)lib/libsensorhubservice.so:system/lib/libsensorhubservice.so \
-    $(LOCAL_PATH)lib/libsensorlistener.so:system/lib/libsensorlistener.so \
+    $(LOCAL_PATH)lib/libaudio-ril.so:system/lib/libaudio-ril.so \
+    $(LOCAL_PATH)lib/libreference-ril.so:system/lib/libreference-ril.so \
+    $(LOCAL_PATH)lib/librilutils.so:system/lib/librilutils.so \	
+    $(LOCAL_PATH)lib/libsecnativefeature.so:system/lib/libsecnativefeature.so
+	
+#==/ Sensor /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)lib/hw/sensorhubs.universal7870.so:system/lib/hw/sensorhubs.universal7870.so \
+    $(LOCAL_PATH)lib/hw/sensorhubs.universal7870.so:system/lib/hw/sensorhubs.universal7870.so \
     $(LOCAL_PATH)lib/libsensorservice.so:system/lib/libsensorservice.so \
-    $(LOCAL_PATH)lib/libstainkiller.so:system/lib/libstainkiller.so \
-    $(LOCAL_PATH)lib/libsynaFpSensorTestNwd.so:system/lib/libsynaFpSensorTestNwd.so \
-    $(LOCAL_PATH)lib/libtfa98xx.so:system/lib/libtfa98xx.so \
-    $(LOCAL_PATH)lib/libuniplugin.so:system/lib/libuniplugin.so \
-    $(LOCAL_PATH)lib/libwrappergps.so:system/lib/libwrappergps.so \
-    $(LOCAL_PATH)lib/modules/qca_cld/qca_cld_wlan.ko:system/lib/modules/qca_cld/qca_cld_wlan.ko \
-    $(LOCAL_PATH)vendor/firmware/fimc_is_lib.bin:system/vendor/firmware/fimc_is_lib.bin \
-    $(LOCAL_PATH)vendor/firmware/mfc_fw.bin:system/vendor/firmware/mfc_fw.bin \
-    $(LOCAL_PATH)vendor/firmware/sec_s3nrn80_firmware.bin:system/vendor/firmware/sec_s3nrn80_firmware.bin \
-    $(LOCAL_PATH)vendor/firmware/setfile_imx219.bin:system/vendor/firmware/setfile_imx219.bin \
-    $(LOCAL_PATH)vendor/firmware/setfile_imx258.bin:system/vendor/firmware/setfile_imx258.bin \
-    $(LOCAL_PATH)vendor/lib/hw/flp.default.so:system/vendor/lib/hw/flp.default.so \
-    $(LOCAL_PATH)vendor/lib/libAutoEnhance.so:system/vendor/lib/libAutoEnhance.so \
-    $(LOCAL_PATH)vendor/lib/libHpr_LocFace_mvfd_v1.0.so:system/vendor/lib/libHpr_LocFace_mvfd_v1.0.so \
-    $(LOCAL_PATH)vendor/lib/libHpr_LocLandmark_sdm_v1.0.so:system/vendor/lib/libHpr_LocLandmark_sdm_v1.0.so \
-    $(LOCAL_PATH)lib/libMcClient.so:system/lib/libMcClient.so \
-    $(LOCAL_PATH)lib/libMcRegistry.so:system/lib/libMcRegistry.so \
-    $(LOCAL_PATH)lib/libOpenCv.so:system/lib/libOpenCv.so \
-    $(LOCAL_PATH)vendor/lib/libPhotoEnhance.so:system/vendor/lib/libPhotoEnhance.so \
-    $(LOCAL_PATH)vendor/lib/libSEF.so:system/vendor/lib/libSEF.so \
-    $(LOCAL_PATH)vendor/lib/libflp.so:system/vendor/lib/libflp.so \
-    $(LOCAL_PATH)lib/libsomp.so:system/lib/libsomp.so \
-    $(LOCAL_PATH)vendor/lib/mediadrm/libdrmclearkeyplugin.so:system/vendor/lib/mediadrm/libdrmclearkeyplugin.so \
-    $(LOCAL_PATH)vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so
+    $(LOCAL_PATH)lib/libsensorlistener.so:system/lib/libsensorlistener.so \
+    $(LOCAL_PATH)lib/libsensorhubservice.so:system/lib/libsensorhubservice.so \
+    $(LOCAL_PATH)lib/hw/sensors.universal7870.so:system/lib/hw/sensors.universal7870.so \
+    $(LOCAL_PATH)lib/libsensorhub.so:system/lib/libsensorhub.so \
+    $(LOCAL_PATH)bin/sensorhubservice:system/bin/sensorhubservice
+
+#==/ NFC /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)etc/sec_s3nrn80_rfreg.bin:system/etc/sec_s3nrn80_rfreg.bin \
+    $(LOCAL_PATH)lib/hw/nfc_nci.universal7870.so:system/lib/hw/nfc_nci.universal7870.so \
+    $(LOCAL_PATH)vendor/firmware/sec_s3nrn80_firmware.bin:system/vendor/firmware/sec_s3nrn80_firmware.bin
+
+#==/ Shim Libs /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)lib/libprotobuf-cpp-N.so:system/lib/libprotobuf-cpp-N.so \
+    $(LOCAL_PATH)lib/libprotobuf-cpp-fooo.so:system/lib/libprotobuf-cpp-fooo.so
+	
+#==/ Surface Composer /==#
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)lib/hw/gralloc.exynos5.so:system/lib/hw/gralloc.exynos5.so
